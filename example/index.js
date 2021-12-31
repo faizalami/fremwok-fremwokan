ef01754a2a25731b641da1696c9700c10bc88e53
Faizal Amiruddin <faizal@wowrack.com>
2021-12-31 19:33:17 +0700

    feat(docs): rollback example

--- example/index.js
+++ example/index.js
@@ -1,84 +1,40 @@
-// import 'regenerator-runtime';
-// import './style.css';
-// import Fw from '../src/Fw';
-// import Router from '../src/Router';
-// import { routes } from './routes';
-//
-// const Route = new Router(routes);
-//
-// const component = {
-//   render () {
-//     return (
-//       <main>
-//         <nav>
-//           <ul>
-//             <li>
-//               <a attrs={{
-//                 href: '#',
-//               }}>Home</a>
-//             </li>
-//             <li>
-//               <a attrs={{
-//                 href: '#/calculator',
-//               }}>Calculator</a>
-//             </li>
-//           </ul>
-//         </nav>
-//
-//         <div attrs={{
-//           class: 'container',
-//         }}>
-//           <Route />
-//         </div>
-//       </main>
-//     );
-//   },
-// };
-//
-// new Fw(component, document.body, {
-//   logger: ['dependency'],
-// });
-
+import 'regenerator-runtime';
+import './style.css';
 import Fw from '../src/Fw';
+import Router from '../src/Router';
+import { routes } from './routes';
+
+const Route = new Router(routes);
 
 const component = {
-  data: {
-    quantity: 10,
-  },
-  // computed: {
-  //   lebihKurang () {
-  //     return this.data.quantity > 10 ? 'Lebih' : 'Kurang';
-  //   },
-  // },
-  methods: {
-    lebihKurang (brp) {
-      console.log('quantity called');
-      // alert(this.data.quantity);
-      return this.data.quantity > brp ? 'Lebih' : 'Kurang';
-    },
-  },
-  // render () {
-  //   return (
-  //     <div>
-  //       <p>
-  //         Total: {this.computed.lebihKurang}
-  //       </p>
-  //     </div>
-  //   );
-  // },
   render () {
     return (
-      <div>
-        <p>
-          Total: {this.methods.lebihKurang(10)}
-        </p>
-      </div>
+      <main>
+        <nav>
+          <ul>
+            <li>
+              <a attrs={{
+                href: '#',
+              }}>Home</a>
+            </li>
+            <li>
+              <a attrs={{
+                href: '#/calculator',
+              }}>Calculator</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div attrs={{
+          class: 'container',
+        }}>
+          <Route />
+        </div>
+      </main>
     );
   },
 };
 
-const fw = new Fw(component, document.body, {
-  // logger: ['computed', 'methods', 'dependency'],
-  logger: ['state', 'dependency'],
+new Fw(component, document.body, {
+  logger: ['dependency'],
 });
-window.fwData = fw;
